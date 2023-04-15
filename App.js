7dc20858bc3b24f4d53dc986ae6f5e1bb8a218df
Daniil Borysov <danofu13@gmail.com>
2023-04-15 20:00:13 +0200

    Setup navigation

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AddPlace from 'screens/AddPlace';
+import AllPlaces from 'screens/AllPlaces';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen component={AllPlaces} name="AllPlaces" />
+          <Stack.Screen component={AddPlace} name="AddPlace" />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
