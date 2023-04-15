5389e0e4341581f4db4051b2a98049a317d17aea
Daniil Borysov <danofu13@gmail.com>
2023-04-15 20:15:24 +0200

    Add "add" button to AllPlaces screen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AddPlace from 'screens/AddPlace';
 import AllPlaces from 'screens/AllPlaces';
+import IconButton from 'components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,10 +14,15 @@ export default function App() {
       <StatusBar />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen component={AllPlaces} name="AllPlaces" />
+          <Stack.Screen component={AllPlaces} name="AllPlaces" options={{ headerRight: AllPlacesHeaderRight }} />
           <Stack.Screen component={AddPlace} name="AddPlace" />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
+}
+
+function AllPlacesHeaderRight({ tintColor }) {
+  const { navigate } = useNavigation();
+  return <IconButton color={tintColor} icon="add" onPress={() => navigate('AddPlace')} size={24} />;
 }
---
+++ components/UI/IconButton.js
@@ -0,0 +1,24 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Pressable, StyleSheet } from 'react-native';
+
+function IconButton({ color, icon, onPress, size }) {
+  return (
+    <Pressable onPress={onPress} style={({ pressed }) => [styles.button, pressed && styles.pressed]}>
+      <Ionicons color={color} name={icon} size={size} />
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 4,
+    padding: 8,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
