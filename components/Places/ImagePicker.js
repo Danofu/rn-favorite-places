83cc6e8e7fbc1bd21f8f6e01a4b485d35bfd4624
Daniil Borysov <danofu13@gmail.com>
2023-04-19 18:48:55 +0200

    Check and ask for camera permission

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,10 +1,31 @@
-import { launchCameraAsync } from 'expo-image-picker';
-import { View, Text, Button } from 'react-native';
+import { Alert, Button, View } from 'react-native';
+import { PermissionStatus, launchCameraAsync, useCameraPermissions } from 'expo-image-picker';
 
 function ImagePicker() {
+  const [cameraPermissionInfo, requestPermission] = useCameraPermissions();
+
+  const verifyPermissions = async () => {
+    if (cameraPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInfo.status === PermissionStatus.DENIED) {
+      Alert.alert('Insufficient Permissions !', 'You need to grand camera permissions to use this app.');
+      return false;
+    }
+
+    return true;
+  };
+
   const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({ allowsEditing: true, aspect: [16, 9], quality: 0.5 });
-    console.log('[ ImagePicker ]: ', image);
+    console.log('[ ImagePicker ]:', image);
   };
 
   return (
