83996b3429f82d536ae8f6be87aadf64f5e44be2
Daniil Borysov <danofu13@gmail.com>
2023-04-21 14:57:43 +0200

    Create insert places util function

--- util/database.js
+++ util/database.js
@@ -2,24 +2,38 @@ import * as SQLite from 'expo-sqlite';
 
 const database = SQLite.openDatabase('places.db');
 
-export const init = () => {
-  const promise = new Promise((resolve, reject) => {
-    database.transaction((transaction) => {
+export const init = () =>
+  new Promise((resolve, reject) => {
+    database.transaction((transaction) =>
       transaction.executeSql(
-        `CREATE TABLE IF NOT EXISTS places (
-          id INTEGER PRIMARY KEY NOT NULL,
-          address, TEXT NOT NULL,
-          imageUri TEXT NOT NULL,
-          lat REAL NOT NULL,
-          lng REAL NOT NULL,
-          title TEXT NOT NULL
-        );`,
+        `
+          CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY NOT NULL,
+            address, TEXT NOT NULL,
+            imageUri TEXT NOT NULL,
+            lat REAL NOT NULL,
+            lng REAL NOT NULL,
+            title TEXT NOT NULL
+          );
+        `,
         [],
         () => resolve(),
         (_, error) => reject(error)
-      );
-    });
+      )
+    );
   });
 
-  return promise;
-};
+export const insertPlace = (place) =>
+  new Promise((resolve, reject) => {
+    database.transaction((transaction) =>
+      transaction.executeSql(
+        `
+          INSERT INTO places (address, imageUri, lat, lng, title)
+          VALUES (?, ?, ?, ?, ?)
+        `,
+        [place.address, place.imageUri, place.lat, place.lng, place.title],
+        (_, result) => resolve(result),
+        (_, error) => reject(error)
+      )
+    );
+  });
