600ab19e2688f1f95ad75146b2c016d0633328ac
Daniil Borysov <danofu13@gmail.com>
2023-04-21 12:27:06 +0200

    Set picked location as Marker

--- screens/Map.js
+++ screens/Map.js
@@ -1,9 +1,28 @@
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const region = { latitude: 37.78, longitude: -122.43, latitudeDelta: 0.0922, longitudeDelta: 0.0421 };
-  return <MapView initialRegion={region} style={styles.map} />;
+
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ lat, lng });
+  };
+
+  return (
+    <MapView initialRegion={region} onPress={selectLocationHandler} style={styles.map}>
+      {selectedLocation && (
+        <Marker
+          coordinate={{ latitude: selectedLocation.lat, longitude: selectedLocation.lng }}
+          title="Picked Location"
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
